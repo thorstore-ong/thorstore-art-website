3bc1b3521e37ae178950745110f7976069e16170
Thoriso Mokhethi <Thoriso.Mokhethi@iotnxt.com>
2026-06-25 11:13:09 +0200

    added the host for where my images will come from

--- thorstore-web/next.config.ts
+++ thorstore-web/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+
+    ],
+  },
 };
 
 export default nextConfig;
